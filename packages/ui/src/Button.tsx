0f339f70fdfcd57303afcc784f71e51c9a7394f7
nerdchanii <nerdchanii@gmail.com>
2023-01-22 23:37:14 +0900

    feat(ui): change Button Props
    - add props size, priority
    *breaking chagne

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,17 +1,19 @@
 import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react';
+import './css/Button.css';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
   onClick: MouseEventHandler<HTMLButtonElement>;
-}
+  size: 'sm' | 'md' | 'lg' | 'xl';
+  priority: 'primary' | 'secondary' | 'thirtiory';
+};
 
-
-const Button = ({ children, onClick, ...props }: ButtonProps) => {
+const Button = ({ children, onClick, size, priority, className, ...props }: ButtonProps) => {
   return (
-    <button onClick={onClick} {...props}>
+    <button onClick={onClick} {...props} className={[size, className, priority].join(' ')}>
       {children}
     </button>
-  )
-}
+  );
+};
 
 export default Button;
