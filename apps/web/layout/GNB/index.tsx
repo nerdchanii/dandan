0ce02b226de416fe07cb264e6e68543e4c661d48
nerdchanii <nerdchanii@gmail.com>
2023-01-27 23:50:33 +0900

    fix(web): reflect update of UI

--- apps/web/layout/GNB/index.tsx
+++ apps/web/layout/GNB/index.tsx
@@ -9,8 +9,12 @@ const index = () => {
       <div>모의</div>
       <NavBar className={styles.navbar} />
       <div className={styles.buttongroup}>
-        <Button onClick={console.log}>로그인</Button>
-        <Button onClick={console.log}>회원가입</Button>
+        <Button size="lg" priority="primary" onClick={console.log}>
+          로그인
+        </Button>
+        <Button size="lg" priority="secondary" onClick={console.log}>
+          회원가입
+        </Button>
       </div>
     </div>
   );
